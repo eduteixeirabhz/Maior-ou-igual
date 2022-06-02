1767dc910f4a9c0bed0e74dbdfa75319eb23fed8
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 15:13:19 -0300

    Add files via upload

---
+++ Desafio038.py
@@ -0,0 +1,9 @@
+pri = int(input('Digite o primeiro número inteiro: '))
+seg = int(input('Digite o segundo número inteiro:  '))
+if pri > seg:
+    print('O PRIMEIRO número é maior que o segundo')
+elif seg > pri:
+    print('O SEGUNDO número é maior que o primeiro ')
+else:
+    print('Os dois números são IGUAIS')
+    
